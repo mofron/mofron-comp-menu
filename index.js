cffbe3c95393bef0520b3ccb4a557bbdb453a8b6
simpart <you@example.com>
2017-02-15 18:23:19 +0900

    mod default size

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ mofron.comp.Menu = class extends mofron.Component {
             
             this.sel_evt = function(){};
             this.sel_idx = null;
+            this.m_size  = new Array(30,150);
             
             if (null !== opt) {
                 this.option(opt);
@@ -77,9 +78,62 @@ mofron.comp.Menu = class extends mofron.Component {
         }
     }
     
+    size (hei, wid) {
+        try {
+            if ((undefined === hei) && (undefined === wid)) {
+                /* getter */
+                return this.m_size;
+            }
+            /* setter */
+            if (('string' !== typeof hei) && ('number' !== typeof hei)) {
+                throw new Error('invalid parameter');
+            }
+            if (('string' !== typeof wid) && ('number' !== typeof wid)) {
+                throw new Error('invalid parameter');
+            }
+            
+            var chd = this.getChild();
+            for (var chd_idx in chd) {
+                this.sizeComp(chd[chd_idx],hei,wid);
+            }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    sizeComp (cmp, hei, wid) {
+        try {
+            var set_hei = hei;
+            var set_wid = wid;
+            if ('number' === typeof hei) {
+                set_hei = hei + 'px';
+            }
+            if ('number' === typeof wid) {
+                set_wid = wid + 'px';
+            }
+            
+            if ( ('function' === typeof cmp['height']) &&
+                 ('function' === typeof cmp['width'])) {
+                cmp.height(set_hei);
+                cmp.width(set_wid);
+            } else {
+                cmp.style('height', set_hei);
+                cmp.style('width' , set_wid);
+            }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
     addChild(comp, disp) {
         try {
             super.addChild(comp, disp);
+            
+            var size = this.size();
+            this.sizeComp(comp, size[0],size[1]);
+            
             comp.addEvent(
                 new mofron.event.Click(function(evt) {
                     try {
